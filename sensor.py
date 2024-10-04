1e7d388b878b7d033decc744e7fa8567acc4f742
DanielBradac <daniel.bradac@email.cz>
2024-10-04 20:03:26 +0200

    updating i2c interface to softi2c

--- sensor.py
+++ sensor.py
@@ -1,7 +1,7 @@
 import GPIO
 
 from BME280 import BME280
-from machine import Pin, I2C
+from machine import Pin, SoftI2C
 
 # Data class for data pulled out of sensor
 class SensorData:
@@ -15,7 +15,7 @@ class SensorData:
         }
 
 # ESP32 - Pin assignment
-i2c = I2C(scl=Pin(GPIO.D1), sda=Pin(GPIO.D0), freq=10000)
+i2c = SoftI2C(scl=Pin(GPIO.D1), sda=Pin(GPIO.D0), freq=10000)
 bme = BME280(i2c=i2c)
 
 # Function reads temperature and humidity from sensor
